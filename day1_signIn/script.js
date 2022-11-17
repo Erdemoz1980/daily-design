8fcd80c65a27219a25eecdbee1b6db403ffe9019
Erdem Ozdemir <erdemozproductions@gmail.com>
2022-11-16 21:49:42 -0500

    cleaned up JS file

--- day1_signIn/script.js
+++ day1_signIn/script.js
@@ -1,13 +1,10 @@
-let groupChecks = document.getElementsByName('gender-check');
-let maleCheck = document.getElementById('male');
-let femaleCheck = document.getElementById('female');
+const maleCheck = document.getElementById('male');
+const femaleCheck = document.getElementById('female');
 
-maleCheck.onchange = ()=>{
+maleCheck.onchange = () => {
   femaleCheck.checked = false;
 }
 
 femaleCheck.onchange = () => {
   maleCheck.checked = false;
 }
-
-
