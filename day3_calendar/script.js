f8f963c93c1d8695306ef760a30af73c458c1f67
Erdem Ozdemir <erdemozproductions@gmail.com>
2022-11-23 16:10:17 -0500

    add to localstorage

--- day3_calendar/script.js
+++ day3_calendar/script.js
@@ -2,77 +2,155 @@ const modalContainer = document.getElementById('modal-container');
 const closeModalBtn = document.getElementById('btn-modal-close');
 const openModalBtn = document.getElementById('add-event-btn');
 const addToCalendarBtn = document.getElementById('add-to-calendar');
-
 const detailsContainer = document.getElementById('details-flex-container');
-
-const calendarItem = document.getElementById('details-item');
+//Inputs
 const dateInput = document.getElementById('date');
 const timeInput = document.getElementById('time');
 const detailsInput = document.getElementById('details');
 
+const currTime = `${new Date().getHours()}:${new Date().getMinutes()}`;
+timeInput.value = currTime
+
+//Check Localstorage on bootup and populate UI
+
+document.addEventListener('DOMContentLoaded', getItemsFromStorage);
+
+
+function getItemsFromStorage() {
+  const savedEvents = localStorage.getItem('events')
+    && JSON.parse(localStorage.getItem('events'));
+  if (savedEvents) {
+    savedEvents.map(event => {  
+      //Create the HTML Elements
+      const newDetailsItem = document.createElement('div');
+      const newIcon = document.createElement('i');
+      const detailsTextCtnr = document.createElement('div');
+      const detailsTitle = document.createElement('h1');
+      const detailsDate = document.createElement('span');
+      const detailsTime = document.createElement('span');
+      const editDeleteCtnr = document.createElement('div');
+      const editBtn = document.createElement('i');
+      const delBtn = document.createElement('i');
+      const idContainer = document.createElement('span');
+
+     //Add Class names
+     newDetailsItem.classList.add('details-item')
+     newIcon.classList.add('fa-solid', 'fa-list');
+     detailsTextCtnr.classList.add('details-text')
+     detailsTitle.classList.add('details-title')
+     detailsDate.classList.add('details-date')
+     detailsTime.classList.add('details-time')
+     editDeleteCtnr.classList.add('edit-delete')
+     editBtn.classList.add('edit-btn','btn-sm', 'fa-solid', 'fa-pen-to-square');
+     delBtn.classList.add('del-btn', 'btn-sm', 'fa-solid', 'fa-trash')
+     idContainer.classList.add('id-container') 
+
+    //Add Content  
+    detailsTitle.innerText = event.detailsTitle
+    detailsDate.innerText = event.detailsDate
+    detailsTime.innerText = event.detailsTime
+    const randomId = Date.now()
+    idContainer.innerText = randomId  
+    
+    //Append Elements to DOM
+    detailsTextCtnr.appendChild(detailsTitle)
+    detailsTextCtnr.appendChild(detailsDate)
+    detailsTextCtnr.appendChild(detailsTime)
+
+    editDeleteCtnr.appendChild(editBtn)
+    editDeleteCtnr.appendChild(delBtn)
+
+    newDetailsItem.appendChild(newIcon)
+    newDetailsItem.appendChild(detailsTextCtnr)
+    newDetailsItem.appendChild(editDeleteCtnr)
+    newDetailsItem.appendChild(idContainer)
+    detailsContainer.appendChild(newDetailsItem)
+   })
+  } 
+  return
+};
+
+//Delete an Event
+detailsContainer.addEventListener('click', editDelete);
+function editDelete(e) {
+  const item = e.target
+  //Delete Event
+  if (item.classList[0] === 'del-btn') {
+    const btnGroup = item.parentElement;
+    const calendarEvent = btnGroup.parentElement;
+    const savedEvents = JSON.parse(localStorage.getItem('events'))
+    console.log(calendarEvent)
+
+    //calendarEvent.remove();
+  }
+  //Edit Event
+};
+
+function saveToLocalStorage(event) {
+  const events = localStorage.getItem('events') ?
+    JSON.parse(localStorage.getItem('events')) : []
+  events.push(event);
+  localStorage.setItem('events', JSON.stringify(events))
+};
 
 function addCalendarEvent() { 
-  console.log('Button working?')
+ //Create the HTML structure
+ const newDetailsItem = document.createElement('div')
+ const newIcon = document.createElement('i')
+ const detailsTextCtnr = document.createElement('div')
+ const detailsTitle = document.createElement('h1')
+ const detailsDate = document.createElement('span')
+ const detailsTime = document.createElement('span')
+ const editDeleteCtnr = document.createElement('div')
+ const editBtn = document.createElement('i')
+const delBtn = document.createElement('i')
+const idContainer = document.createElement('span')
 
-  //Create the HTML structure
-  const newDetailsItem = document.createElement('div');
-  const newIcon = document.createElement('i');
-  const detailsTextCtnr = document.createElement('div');
-  const detailsTitle = document.createElement('h1');
-  const detailsDate = document.createElement('span');
-  const detailsTime = document.createElement('span');
-  const editDeleteCtnr = document.createElement('div');
-  const editBtn = document.createElement('button');
-  const delBtn = document.createElement('button');
+ //Add Class names
+ newDetailsItem.classList.add('details-item')
+ newIcon.classList.add('fa-solid', 'fa-list')
+ detailsTextCtnr.classList.add('details-text')
+ detailsTitle.classList.add('details-title')
+ detailsDate.classList.add('details-date')
+ detailsTime.classList.add('details-time')
+ editDeleteCtnr.classList.add('edit-delete')
+ editBtn.classList.add('edit-btn','btn-sm', 'fa-solid', 'fa-pen-to-square')
+ delBtn.classList.add('del-btn', 'btn-sm', 'fa-solid', 'fa-trash')
+idContainer.classList.add('id-container') 
 
-  //Add Class names
-  newDetailsItem.classList.add('details-item')
-  newIcon.classList.add('fa-solid', 'fa-list');
-  detailsTextCtnr.classList.add('details-text')
-  detailsTitle.classList.add('details-title')
-  detailsDate.classList.add('details-date')
-  detailsTime.classList.add('details-time')
-  editDeleteCtnr.classList.add('edit-delete')
-  editBtn.classList.add('btn-sm')
-  editBtn.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
-  delBtn.classList.add('btn-sm')
-  delBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
-
-  //Add Text Content
-  detailsTitle.innerText = detailsInput.value;
-  detailsDate.innerText = dateInput.value;
-  detailsTime.innerText = timeInput.value;
-
-
+  //Retrieve input values
+ detailsTitle.innerText = detailsInput.value
+ detailsDate.innerText = dateInput.value
+ detailsTime.innerText = timeInput.value
+ const randomId = Date.now()
+ idContainer.innerText = randomId  
+   
   //Append Elements to DOM
-  detailsTextCtnr.appendChild(detailsTitle);
-  detailsTextCtnr.appendChild(detailsDate);
-  detailsTextCtnr.appendChild(detailsTime);
-
-  editDeleteCtnr.appendChild(editBtn);
-  editDeleteCtnr.appendChild(delBtn);
-
-  newDetailsItem.appendChild(newIcon);
-  newDetailsItem.appendChild(detailsTextCtnr);
-  newDetailsItem.appendChild(editDeleteCtnr);
-
+  detailsTextCtnr.appendChild(detailsTitle)
+  detailsTextCtnr.appendChild(detailsDate)
+  detailsTextCtnr.appendChild(detailsTime)
+  editDeleteCtnr.appendChild(editBtn)
+  editDeleteCtnr.appendChild(delBtn)
+  newDetailsItem.appendChild(newIcon)
+  newDetailsItem.appendChild(detailsTextCtnr)
+  newDetailsItem.appendChild(editDeleteCtnr)
+  newDetailsItem.appendChild(idContainer)
   detailsContainer.appendChild(newDetailsItem)
 
+ //Add Event to Localstorage
+ saveToLocalStorage({
+   detailsTitle: detailsInput.value,
+   detailsDate: dateInput.value, 
+   detailsTime:timeInput.value
+ });
 
-
-  //Reset Input Boxes
-  dateInput.value = '';
-  timeInput.value = '';
-  detailsInput.value = '';
-
-}
+};
 
 addToCalendarBtn.onclick = function (e) {
   e.preventDefault()
   addCalendarEvent()
   modalContainer.style.display = 'none';
 }; 
-
 
 // Open Modal
 openModalBtn.onclick = function (e) {
