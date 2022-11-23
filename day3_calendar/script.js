5b50ab842d8a33c2fbdd16536bc5df252f10ace1
Erdem Ozdemir <erdemozproductions@gmail.com>
2022-11-22 21:07:14 -0500

    add item functinality

--- day3_calendar/script.js
+++ day3_calendar/script.js
@@ -3,11 +3,75 @@ const closeModalBtn = document.getElementById('btn-modal-close');
 const openModalBtn = document.getElementById('add-event-btn');
 const addToCalendarBtn = document.getElementById('add-to-calendar');
 
-const detailsFlexContainer = document.getElementById('details-flex-container')
+const detailsContainer = document.getElementById('details-flex-container');
 
+const calendarItem = document.getElementById('details-item');
 const dateInput = document.getElementById('date');
 const timeInput = document.getElementById('time');
 const detailsInput = document.getElementById('details');
+
+
+function addCalendarEvent() { 
+  console.log('Button working?')
+
+  //Create the HTML structure
+  const newDetailsItem = document.createElement('div');
+  const newIcon = document.createElement('i');
+  const detailsTextCtnr = document.createElement('div');
+  const detailsTitle = document.createElement('h1');
+  const detailsDate = document.createElement('span');
+  const detailsTime = document.createElement('span');
+  const editDeleteCtnr = document.createElement('div');
+  const editBtn = document.createElement('button');
+  const delBtn = document.createElement('button');
+
+  //Add Class names
+  newDetailsItem.classList.add('details-item')
+  newIcon.classList.add('fa-solid', 'fa-list');
+  detailsTextCtnr.classList.add('details-text')
+  detailsTitle.classList.add('details-title')
+  detailsDate.classList.add('details-date')
+  detailsTime.classList.add('details-time')
+  editDeleteCtnr.classList.add('edit-delete')
+  editBtn.classList.add('btn-sm')
+  editBtn.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
+  delBtn.classList.add('btn-sm')
+  delBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
+
+  //Add Text Content
+  detailsTitle.innerText = detailsInput.value;
+  detailsDate.innerText = dateInput.value;
+  detailsTime.innerText = timeInput.value;
+
+
+  //Append Elements to DOM
+  detailsTextCtnr.appendChild(detailsTitle);
+  detailsTextCtnr.appendChild(detailsDate);
+  detailsTextCtnr.appendChild(detailsTime);
+
+  editDeleteCtnr.appendChild(editBtn);
+  editDeleteCtnr.appendChild(delBtn);
+
+  newDetailsItem.appendChild(newIcon);
+  newDetailsItem.appendChild(detailsTextCtnr);
+  newDetailsItem.appendChild(editDeleteCtnr);
+
+  detailsContainer.appendChild(newDetailsItem)
+
+
+
+  //Reset Input Boxes
+  dateInput.value = '';
+  timeInput.value = '';
+  detailsInput.value = '';
+
+}
+
+addToCalendarBtn.onclick = function (e) {
+  e.preventDefault()
+  addCalendarEvent()
+  modalContainer.style.display = 'none';
+}; 
 
 
 // Open Modal
@@ -28,52 +92,6 @@ window.onclick = function (e) {
 };
 
 
-addToCalendarBtn.onclick = function (e) {
-  e.preventDefault();
-  const newDetailsItem = document.createElement('div');
-  const newIcon = document.createElement('i');
-  const detailsTextCtnr = document.createElement('div');
-  const detailsTitle = document.createElement('h1');
-  const detailsDate = document.createElement('span');
-  const detailsTime = document.createElement('span');
-  const editDeleteCtnr = document.createElement('div');
-  const editBtn = document.createElement('button');
-  const delBtn = document.createElement('button');
-
-  newDetailsItem.classList = 'details-item';
-  newIcon.classList = 'fa-solid fa-magnifying-glass';
-  detailsTextCtnr.classList = 'details-text';
-  
-  detailsTitle.innerText = detailsInput.value;
-  detailsTitle.className = 'details-title';
-
-  detailsDate.innerText = dateInput.value;
-  detailsDate.className = 'details-date';
-
-  detailsTime.innerText = timeInput.value;
-  detailsTime.classList = 'details-time';
-  
-  editDeleteCtnr.classList = 'edit-delete';
-  editBtn.className = 'edit-btn';
-  delBtn.className = 'delete-btn';
-
-  detailsTextCtnr.appendChild(detailsTitle);
-  detailsTextCtnr.appendChild(detailsDate);
-  detailsTextCtnr.appendChild(detailsTime);
-
-  editDeleteCtnr.appendChild(editBtn);
-  editDeleteCtnr.appendChild(delBtn);
-
-  newDetailsItem.appendChild(newIcon);
-  newDetailsItem.appendChild(detailsTextCtnr);
-  newDetailsItem.appendChild(editDeleteCtnr);
-  detailsFlexContainer.appendChild(newDetailsItem);
-
-  dateInput.value = '';
-  timeInput.value = '';
-  detailsInput.value = '';
-  
-};
 
 
 
