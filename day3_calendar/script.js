168eb62f10b5f6e46bb460d22ab2597d072d4bc7
Erdem Ozdemir <erdemozproductions@gmail.com>
2022-11-24 14:44:56 -0500

    edit event done

--- day3_calendar/script.js
+++ day3_calendar/script.js
@@ -1,40 +1,41 @@
-const modalContainer = document.getElementById('modal-container');
-const closeModalBtn = document.getElementById('btn-modal-close');
-const openModalBtn = document.getElementById('add-event-btn');
-const addToCalendarBtn = document.getElementById('add-to-calendar');
-const detailsContainer = document.getElementById('details-flex-container');
+const modalContainer = document.getElementById('modal-container')
+const deleteModalContainer = document.getElementById('modal-container-delete')
+const closeModalBtn = document.getElementById('btn-modal-close')
+const openModalBtn = document.getElementById('add-event-btn')
+const confirmDelBtn = document.getElementById('confirm-delete')
+const declineDelBtn = document.getElementById('decline-delete')
+const addToCalendarBtn = document.getElementById('add-to-calendar')
+const detailsContainer = document.getElementById('details-flex-container')
 //Inputs
-const dateInput = document.getElementById('date');
-const timeInput = document.getElementById('time');
-const detailsInput = document.getElementById('details');
+const dateInput = document.getElementById('date')
+const timeInput = document.getElementById('time')
+const detailsInput = document.getElementById('details')
 
-const currTime = `${new Date().getHours()}:${new Date().getMinutes()}`;
+const currTime = `${new Date().getHours()}:${new Date().getMinutes()}`
 timeInput.value = currTime
 
 //Check Localstorage on bootup and populate UI
 
-document.addEventListener('DOMContentLoaded', getItemsFromStorage);
+document.addEventListener('DOMContentLoaded', getItemsFromStorage)
 
 function getItemsFromStorage() {
   const savedEvents = localStorage.getItem('events')
-    && JSON.parse(localStorage.getItem('events'));
+    && JSON.parse(localStorage.getItem('events'))
   if (savedEvents) {
     savedEvents.map(event => {  
       //Create the HTML Elements
-      const newDetailsItem = document.createElement('div');
-      const newIcon = document.createElement('i');
-      const detailsTextCtnr = document.createElement('div');
-      const detailsTitle = document.createElement('h1');
-      const detailsDate = document.createElement('span');
-      const detailsTime = document.createElement('span');
-      const editDeleteCtnr = document.createElement('div');
-      const editBtn = document.createElement('i');
-      const delBtn = document.createElement('i');
-      const idContainer = document.createElement('span');
-
+      const newDetailsItem = document.createElement('div')
+      const newIcon = document.createElement('i')
+      const detailsTextCtnr = document.createElement('div')
+      const detailsTitle = document.createElement('h1')
+      const detailsDate = document.createElement('span')
+      const detailsTime = document.createElement('span')
+      const editDeleteCtnr = document.createElement('div')
+      const editBtn = document.createElement('i')
+      const delBtn = document.createElement('i')
 
      //Add Class names
-     newDetailsItem.classList.add('details-item')
+     newDetailsItem.classList.add('details-item',`${event.id}`)
      newIcon.classList.add('fa-solid', 'fa-list');
      detailsTextCtnr.classList.add('details-text')
      detailsTitle.classList.add('details-title')
@@ -43,14 +44,12 @@ function getItemsFromStorage() {
      editDeleteCtnr.classList.add('edit-delete')
      editBtn.classList.add('edit-btn','btn-sm', 'fa-solid', 'fa-pen-to-square');
      delBtn.classList.add('del-btn', 'btn-sm', 'fa-solid', 'fa-trash')
-     idContainer.className = 'id-container'
-
 
     //Add Content  
     detailsTitle.innerText = event.detailsTitle
     detailsDate.innerText = event.detailsDate
     detailsTime.innerText = event.detailsTime
-    idContainer.innerText = event.id
+
   
     //Append Elements to DOM
     detailsTextCtnr.appendChild(detailsTitle)
@@ -63,7 +62,6 @@ function getItemsFromStorage() {
     newDetailsItem.appendChild(newIcon)
     newDetailsItem.appendChild(detailsTextCtnr)
     newDetailsItem.appendChild(editDeleteCtnr)
-    newDetailsItem.appendChild(idContainer)
     detailsContainer.appendChild(newDetailsItem)
    })
   } 
@@ -71,39 +69,50 @@ function getItemsFromStorage() {
 };
 
 //Delete or Edit an Event
-detailsContainer.addEventListener('click', editOrDelete);
+detailsContainer.addEventListener('click', editOrDelete)
 
 function editOrDelete(e) {
-  const item = e.target
-  const btnGroup = item.parentElement;
-  const calendarEvent = btnGroup.parentElement;
-  const savedEvents = JSON.parse(localStorage.getItem('events'))
-  const eventId = calendarEvent.children[3].innerText
+  if (e.target.parentElement.className !== 'edit-delete') return;
 
-  //Delete Event
+  const item = e.target
+  const savedEvents = JSON.parse(localStorage.getItem('events'))
+  const btnGroup = item.parentElement
+  const calendarEvent = btnGroup.parentElement
+  const eventId = Number(calendarEvent.classList[1])
+  
+  //Delete || Edit Events
   if (item.classList[0] === 'del-btn') {
-    const updatedEvents = savedEvents.filter(event => event.id !== Number(eventId));
-    localStorage.setItem('events', JSON.stringify(updatedEvents));
-    //Remove from DOM
-    calendarEvent.remove();
+    deleteModalContainer.style.display = 'flex'
+    confirmDelBtn.onclick = ()=> {
+      const updatedEvents = savedEvents.filter(event => event.id !== Number(eventId))
+      localStorage.setItem('events', JSON.stringify(updatedEvents))
+      //Remove from DOM
+      calendarEvent.remove();
+      deleteModalContainer.style.display = 'none'
+    }
+    declineDelBtn.onclick = () => {
+      deleteModalContainer.style.display = 'none'
+    } 
   } else if (item.classList[0] === 'edit-btn') {
     modalContainer.style.display = 'flex'
-    const currEvent = savedEvents.find(event => event.id === Number(eventId))
-    //Populate Modal with currEvent
-    detailsInput.value = currEvent.detailsTitle
-    timeInput.value = currEvent.detailsTime
-    dateInput.value = currEvent.detailsDate
+    addToCalendarBtn.innerText = 'Edit Event'
+    addToCalendarBtn.classList.add(eventId)
 
+       //Populate Modal with currEvent
+       const currEvent = savedEvents.find(event => event.id === eventId)
+       detailsInput.value = currEvent.detailsTitle
+       timeInput.value = currEvent.detailsTime
+       dateInput.value = currEvent.detailsDate
+    
   }
 }
-
 
 function saveToLocalStorage(event) {
   const events = localStorage.getItem('events') ?
     JSON.parse(localStorage.getItem('events')) : []
   events.push(event);
   localStorage.setItem('events', JSON.stringify(events))
-};
+}
 
 function addCalendarEvent() { 
  //Create the HTML structure
@@ -116,9 +125,10 @@ function addCalendarEvent() {
  const editDeleteCtnr = document.createElement('div')
  const editBtn = document.createElement('i')
 const delBtn = document.createElement('i')
-const idContainer =document.createElement('span')
+
  //Add Class names
- newDetailsItem.classList.add('details-item')
+ const id = Date.now()
+ newDetailsItem.classList.add('details-item', `${id}`)
  newIcon.classList.add('fa-solid', 'fa-list')
  detailsTextCtnr.classList.add('details-text')
  detailsTitle.classList.add('details-title')
@@ -127,14 +137,13 @@ const idContainer =document.createElement('span')
  editDeleteCtnr.classList.add('edit-delete')
  editBtn.classList.add('edit-btn','btn-sm', 'fa-solid', 'fa-pen-to-square')
  delBtn.classList.add('del-btn', 'btn-sm', 'fa-solid', 'fa-trash')
-idContainer.className = 'id-container'
+
   //Retrieve input values
  detailsTitle.innerText = detailsInput.value
  detailsDate.innerText = dateInput.value
 detailsTime.innerText = timeInput.value
-idContainer.innerText = Date.now()  
 
-   
+
   //Append Elements to DOM
   detailsTextCtnr.appendChild(detailsTitle)
   detailsTextCtnr.appendChild(detailsDate)
@@ -144,7 +153,6 @@ idContainer.innerText = Date.now()
   newDetailsItem.appendChild(newIcon)
   newDetailsItem.appendChild(detailsTextCtnr)
   newDetailsItem.appendChild(editDeleteCtnr)
-  newDetailsItem.appendChild(idContainer)
   detailsContainer.appendChild(newDetailsItem)
 
  //Add Event to Localstorage
@@ -152,19 +160,56 @@ idContainer.innerText = Date.now()
    detailsTitle: detailsInput.value,
    detailsDate: dateInput.value, 
    detailsTime: timeInput.value,
-   id:Date.now()
+   id
  });
 
 };
 
+function updateEvent() {
+  const eventId = addToCalendarBtn.classList[2]
+   //Update the DOM / UI!!!!!
+  const currEventsInDom = document.querySelectorAll('.details-item');
+  currEventsInDom.forEach(event => {
+    if (event.classList[1] == eventId) {
+      event.children[1].children[0].innerText = detailsInput.value
+      event.children[1].children[1].innerText = dateInput.value
+      event.children[1].children[2].innerText = timeInput.value
+    } else {
+      return event
+    }
+  });
+  //Update localstorage
+  const savedEvents = JSON.parse(localStorage.getItem('events'))
+  const updatedEvents = savedEvents.map(event => {
+    if (event.id === Number(eventId)) {
+      return {
+        ...event,
+        detailsTitle: detailsInput.value,
+        detailsTime: timeInput.value,
+        detailsDate:dateInput.value
+      }
+    } else {
+      return event
+    }
+   })
+  localStorage.setItem('events', JSON.stringify(updatedEvents));
+ 
+
+}
+//Add to Calendar or Edit Event Button
 addToCalendarBtn.onclick = function (e) {
   e.preventDefault()
+  if (addToCalendarBtn.innerText === 'Add to Calendar') {
   addCalendarEvent()
-  modalContainer.style.display = 'none';
-}; 
+  } else {
+    updateEvent();
+}
+modalContainer.style.display = 'none'
+}
 
 // Open Modal
 openModalBtn.onclick = function (e) {
+  addToCalendarBtn.innerText =  'Add to Calendar'
   modalContainer.style.display = 'flex'
 }
 
@@ -175,8 +220,12 @@ closeModalBtn.onclick = function (e) {
 };
 
 window.onclick = function (e) {
-  if (e.target == modalContainer) {
+  if (e.target === modalContainer) {
     modalContainer.style.display = 'none'
+   
+  }
+  if (e.target === deleteModalContainer) {
+    deleteModalContainer.style.display = 'none'
   }
 };
 
